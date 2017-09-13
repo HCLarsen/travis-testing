4bb1dce870c6450b1b452a7fd580a0eb1bb82d13
Chris Larsen <clarsenipod@gmail.com>
2017-09-13 16:09:16 -0400

    Remove Rails 4

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,5 @@
-rails_version = ENV["RAILS_VERSION"]
-puts "Rails: #{rails_version}"
-
-if rails_version >= "5"
-  puts "Testing Rails 5"
-  require File.expand_path("../../test/dummy5/config/environment.rb", __FILE__)
-  ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy5/db/migrate", __FILE__)]
-else
-  puts "Testing Rails 4.2"
-  require File.expand_path("../../test/dummy4/config/environment.rb", __FILE__)
-  ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy4/db/migrate", __FILE__)]
-end
+require File.expand_path("../../test/dummy/config/environment.rb", __FILE__)
+ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy/db/migrate", __FILE__)]
 require "rails/test_help"
 
 # Filter out Minitest backtrace while allowing backtrace from other libraries
